10174ad8e3b715762e3049d819eb150908b6dfe7
hijack <xiaohaijoe@hotmail.com>
2018-05-28 03:42:25 +0800

    提交应用

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,25 +1,36 @@
 <template>
   <div id="index">
 
+    <div class="line-box" style="top: 10599px; left: -2740.8px; width: 86.26px; height: 46.5697px;">
+      <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="line-svg">
+        <g class="line-svg-path  selected">
+          <path d="M20 20 C 66.25996016546014 20, 20 26.56973987839956, 66.25996016546014 26.56973987839956 "
+                stroke="black" fill="none" class="line-body"></path>
+          <path d="M20 20 C 66.25996016546014 20, 20 26.56973987839956, 66.25996016546014 26.56973987839956 "
+                stroke="black" fill="none" class="line-body-ghost"></path>
+        </g>
+      </svg>
+    </div>
     <div ref="container" class="container"
          v-finger:press-move="pressMoveContainer.bind(this)"
          v-finger:multipoint-start="multipointStart"
          v-finger:pinch="pinch">
 
       <!--<div id="myDiv" ref="myDiv" class="box"-->
-           <!--v-finger:press-move="pressMove.bind(this)">-->
-        <!--the element that you want to bind event-->
+      <!--v-finger:press-move="pressMove.bind(this)">-->
+      <!--the element that you want to bind event-->
       <!--</div>-->
       <div ref="scalebox" style="position:relative">
 
 
         <template v-for="event in eventInfo.eventList">
-            <timeline
-              v-bind:id="'event'+event.eventId"
-              v-bind:style="{left:event.left+'px',top:event.top+'px'}"
-              v-finger:press-move="pressMove.bind(this,event.eventId)"
-              v-finger:touch-end="pressEnd.bind(this)"></timeline>
+          <timeline
+            v-bind:id="'event'+event.eventId"
+            v-bind:style="{left:event.left+'px',top:event.top+'px'}"
+            v-finger:press-move="pressMove.bind(this,event.eventId)"
+            v-finger:touch-end="pressEnd.bind(this)"></timeline>
         </template>
+
       </div>
 
     </div>
@@ -44,19 +55,19 @@
     data() {
       return {
         initScale: 1,
-        isMoving : false,
-        eventInfo : {
-          eventList : [
+        isMoving: false,
+        eventInfo: {
+          eventList: [
             {
-              eventId : 1,
-              left : 100,
-              top:100
+              eventId: 1,
+              left: 100,
+              top: 100
             },
-            {
-              eventId : 2,
-              left : 100,
-              top:100
-            }
+            // {
+            //   eventId : 2,
+            //   left : 100,
+            //   top:100
+            // }
           ]
         }
       }
@@ -66,31 +77,31 @@
 
       // Transform(this.$refs.myDiv)
       Transform(this.$refs.scalebox)
-      for(let i = 0 ; i < this.eventInfo.eventList.length ; i++){
+      for (let i = 0; i < this.eventInfo.eventList.length; i++) {
         let event = this.eventInfo.eventList[i]
-        let el = document.getElementById("event"+event.eventId);
+        let el = document.getElementById("event" + event.eventId);
         Transform(el);
       }
     },
     methods: {
 
-      pressMove: function (eventId,evt) {
-        let obj = document.getElementById("event"+eventId);
+      pressMove: function (eventId, evt) {
+        let obj = document.getElementById("event" + eventId);
         obj.translateX += evt.deltaX;
         obj.translateY += evt.deltaY;
         this.isMoving = true
         evt.preventDefault();
       },
-      pressEnd : function(evt){
+      pressEnd: function (evt) {
         this.isMoving = false
       },
       pressMoveContainer: function (evt) {
-        if(this.isMoving){
+        if (this.isMoving) {
           return
         }
-        for(let i = 0 ; i < this.eventInfo.eventList.length ; i++){
+        for (let i = 0; i < this.eventInfo.eventList.length; i++) {
           let event = this.eventInfo.eventList[i]
-          let el = document.getElementById("event"+event.eventId);
+          let el = document.getElementById("event" + event.eventId);
           el.translateX += evt.deltaX;
           el.translateY += evt.deltaY;
           evt.preventDefault();
@@ -104,16 +115,16 @@
         // for(let i = 0 ; i < this.eventInfo.eventList.length ; i++){
         //   let event = this.eventInfo.eventList[i]
         //   let el = document.getElementById("event"+event.eventId);
-          // el.translateX += evt.deltaX;
-          // el.translateY += evt.deltaY;
+        // el.translateX += evt.deltaX;
+        // el.translateY += evt.deltaY;
         this.$refs.scalebox.scaleX = this.$refs.scalebox.scaleY = this.initScale * evt.zoom;
-          // evt.preventDefault();
+        // evt.preventDefault();
         // }
 
       }
     },
-    components:{
-      'Timeline':Timeline
+    components: {
+      'Timeline': Timeline
     }
   }
 </script>
@@ -131,5 +142,36 @@
     width: 100%;
     height: 100%;
     /*background: red;*/
+  }
+
+
+  .line-box {
+    position: absolute;
+    pointer-events: none;
+    transition: all .3s;
+    outline: 2px dashed rgba(0, 0, 0, 0.12);
+    background: rgba(255, 255, 255, 0.9);
+  }
+  .line-box svg {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
+  }
+  .line-box:not(.ghost) .line-body-ghost, .line-box:not(.ghost) .line-body {
+    pointer-events: auto;
+  }
+
+  .line-box path.line-body {
+    stroke-width: 3px;
+    stroke: #acacac;
+    marker-end: url(#Triangle);
+    /* marker-start: url(#markerCircle); */
+  }
+  .line-box path.line-body-ghost {
+    stroke-width: 40px;
+    stroke: transparent;
   }
 </style>
